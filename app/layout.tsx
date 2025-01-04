8f148d026a9ee7d75099c1f43573059c1b5dc45f
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2025-01-04 18:09:43 +0100

    feat: add initial layout, homepage, and global styles for Next Prompt application

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import "@/styles/globals.css";
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "Next Prompt",
+  description: "Discover and share AI prompts",
+};
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
