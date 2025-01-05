83b5cf497328a6b92fe47ec7488dd01647bcd559
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2025-01-05 14:52:43 +0100

    feat: add next-auth integration boilerplate for user authentication in navigation component, mobile vs desktop view

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,9 +1,25 @@
 'use client';
 
+import { getProviders, signIn, signOut } from 'next-auth/react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 const Nav = () => {
+  const isUserLoggedIn = true;
+
+  const [authProviders, setAuthProviders] = useState<Awaited<
+    ReturnType<typeof getProviders>
+  > | null>(null);
+
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders();
+      setAuthProviders(response);
+    };
+    setProviders();
+  }, []);
+
   return (
     <nav className="flex-between mb-16 w-full pt-3">
       <Link
@@ -19,6 +35,80 @@ const Nav = () => {
         />
         <p className="logo_text">Next Prompt</p>
       </Link>
+
+      {/* Desktop navigation */}
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex-center gap-3 md:gap-5">
+            <Link
+              className="black_btn"
+              href="/create-prompt"
+            >
+              Create Post
+            </Link>
+            <button
+              className="outline_btn"
+              type="button"
+              onClick={() => signOut()}
+            >
+              Sign Out
+            </button>
+            <Link href="/profile">
+              <Image
+                alt="profile"
+                className="rounded-full"
+                height={37}
+                src="/assets/images/logo.svg"
+                width={37}
+              />
+            </Link>
+          </div>
+        ) : (
+          <>
+            {authProviders &&
+              Object.values(authProviders).map(provider => (
+                <button
+                  key={provider.name}
+                  className="black_btn"
+                  type="button"
+                  onClick={() => signIn(provider.id)}
+                >
+                  Sign in
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+
+      {/* Mobile navigation */}
+      <div className="sm:hidden flex relative">
+        {isUserLoggedIn ? (
+          <div className="flex">
+            <Image
+              alt="menu"
+              className="invert-white"
+              height={37}
+              src="/assets/images/logo.svg"
+              width={37}
+              onClick={() => {}}
+            />
+          </div>
+        ) : (
+          <>
+            {authProviders &&
+              Object.values(authProviders).map(provider => (
+                <button
+                  key={provider.name}
+                  className="black_btn"
+                  type="button"
+                  onClick={() => signIn(provider.id)}
+                >
+                  Sign in
+                </button>
+              ))}
+          </>
+        )}
+      </div>
     </nav>
   );
 };
